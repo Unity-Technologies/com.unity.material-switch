10c33d337246356cb1836720107c6bc38027cf49
Simon Wittber <simonwittber@unity3d.com>
2021-07-04 15:49:39 +0800

    sync color changes and texture swaps in inspector

--- Editor/MaterialSwitchClipEditor.cs
+++ Editor/MaterialSwitchClipEditor.cs
@@ -141,7 +141,7 @@ namespace Unity.MaterialSwitch
                 }
             }
         }
-
+        
         void ShowDropDownButton(GenericMenu menu, string buttonLabel)
         {
             var buttonText = new GUIContent(buttonLabel);

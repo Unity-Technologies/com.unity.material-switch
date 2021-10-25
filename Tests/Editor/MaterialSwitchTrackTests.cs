ca98d4381b13377d86993190dfe11e28545b0342
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-25 13:01:03 +0900

    add a test to assign SelectioNGroup

--- Tests/Editor/MaterialSwitchTrackTests.cs
+++ Tests/Editor/MaterialSwitchTrackTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.FilmInternalUtilities;
 using Unity.FilmInternalUtilities.Editor;
+using Unity.SelectionGroups;
+using Unity.SelectionGroups.Runtime;
 using UnityEditor;
 using UnityEditor.Timeline;
 using UnityEngine;
@@ -28,6 +30,45 @@ internal class MaterialSwitchTrackTests
         EditorTestUtility.DestroyTimelineAssets(clip);
     }
 
+//----------------------------------------------------------------------------------------------------------------------    
+    [UnityTest]
+    public IEnumerator AssignSelectionGroupToTrack() {
+        TimelineAsset timelineAsset = TimelineEditorUtility.CreateAsset(MaterialSwitchTestEditorConstants.TEST_TIMELINE_ASSET_PATH);
+        yield return null;
+        
+        PlayableDirector    director = new GameObject("Director").AddComponent<PlayableDirector>();
+        MaterialSwitchTrack track    = timelineAsset.CreateTrack<MaterialSwitchTrack>(null, "TestTrack");
+        director.playableAsset = timelineAsset;
+        yield return null;
+
+        SelectionGroup group = CreateSceneSelectionGroup("New Group", string.Empty, Color.green, new List<Object>());
+        director.SetGenericBinding(track, group);
+        TimelineClip clip = track.CreateDefaultClip();
+        yield return null;
+
+        EditorTestUtility.DestroyTimelineAssets(clip);
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    
+
+    //[TODO-sin: 2021-10-25] make CreateSceneSelectionGroup() into a public API 
+    SelectionGroup CreateSceneSelectionGroup(string name, string query, Color color, IList<Object> members)
+    {
+        var g = new GameObject(name);
+        Undo.RegisterCreatedObjectUndo(g,"New Scene Selection Group");
+        var group = g.AddComponent<Unity.SelectionGroups.Runtime.SelectionGroup>();
+        group.Name        = name;
+        group.Query       = query;
+        group.Color       = color;
+        group.Scope       = SelectionGroupDataLocation.Scene;
+        group.ShowMembers = true;
+        group.Add(members);
+        SelectionGroupManager.Register(group);
+        return group;
+    }
+    
     
 }
 

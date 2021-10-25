b47e5580e6338838fd02c6d113868eaaf7c1b95f
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-10-25 19:53:40 +0900

    fix: errors when adding new MaterialSwitchClips (#51)
    Others:
    * Remove debug logs

--- Editor/MaterialSwitchClipTimelineEditor.cs
+++ Editor/MaterialSwitchClipTimelineEditor.cs
@@ -56,13 +56,11 @@ namespace Unity.MaterialSwitch
             var selectionGroup = TimelineEditor.inspectedDirector.GetGenericBinding(track) as SelectionGroups.Runtime.SelectionGroup;
             if (selectionGroup == null)
             {
-                Debug.LogError("Generic Binding must be a SelectionGroup.");
                 return;
             }
             if (!selectionGroup.TryGetComponent<MaterialGroup>(out MaterialGroup materialPropertyGroup))
             {
                 materialPropertyGroup = selectionGroup.gameObject.AddComponent<MaterialGroup>();
-                Debug.Log("Adding Material Group to Selection Group.");
             }
             if (materialPropertyGroup == null)
             {

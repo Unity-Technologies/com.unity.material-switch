b245f0fc0b760d426fb1129f3e09b8890cc4210e
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-10 14:32:49 +0900

    remove unused functions

--- Tests/Editor/EditorTestUtility.cs
+++ Tests/Editor/EditorTestUtility.cs
@@ -11,28 +11,6 @@ namespace Unity.MaterialSwitch.EditorTests
 internal static class EditorTestUtility
 {
         
-//----------------------------------------------------------------------------------------------------------------------
-    
-    //[TODO-sin:2021-10-25] Move to FIU
-    internal static void Destroy(Object obj, bool forceImmediate = false, bool allowDestroyingAssets = false) {
-        if (!Application.isPlaying || forceImmediate) {
-            Object.DestroyImmediate(obj,allowDestroyingAssets);                        
-        } else {
-            Object.Destroy(obj);            
-        }
-    }
-//----------------------------------------------------------------------------------------------------------------------
-    
-    //[TODO-sin:2021-10-25] Move to FIU
-    internal static IEnumerator WaitForFrames(int numFrames) {
-        for (int i = 0; i < numFrames; ++i) {
-            yield return null;
-            
-        }        
-        Undo.IncrementCurrentGroup();
-    }        
-
-    
 }
 
 } //end namespace

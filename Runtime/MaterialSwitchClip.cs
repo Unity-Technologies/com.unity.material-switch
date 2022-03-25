31561315ec854fbdc90c09d50b7ca3dc748e1375
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-25 13:17:59 +0900

    update Override function to generic

--- Runtime/MaterialSwitchClip.cs
+++ Runtime/MaterialSwitchClip.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Unity.SelectionGroups;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Serialization;
@@ -28,20 +27,30 @@ namespace Unity.MaterialSwitch
         }
 
         /// <summary>
-        /// Override a texture property in a certain material.
+        /// Override a property in a certain material.
         /// </summary>
         /// <param name="mat">The material which has the property.</param>
         /// <param name="propertyName">The name of the property to be overridden</param>
-        /// <param name="tex">The texture for overriding.</param>
+        /// <param name="obj">The object for overriding.</param>
         /// <returns>True if the applicable property is found and overridable, false otherwise.</returns>
-        public bool OverrideTextureProperty(Material mat, string propertyName, Texture2D tex) 
+        public bool OverrideTextureProperty<T>(Material mat, string propertyName, T obj) 
         {
-            MaterialProperties mp = FindMaterialProperties(mat);
-            TextureProperty p = mp?.FindTextureProperty(propertyName);
-            if (p == null)
+            MaterialProperties  mp = FindMaterialProperties(mat);
+            if (null == mp)
                 return false;
 
-            p.targetValue       = tex;
+            MaterialProperty<T> p = null;
+            if (typeof(Texture2D) == typeof(T) ) {
+                 p = mp?.FindTextureProperty(propertyName) as MaterialProperty<T>;
+            } else if (typeof(float) == typeof(T) ) { 
+                p = mp?.FindFloatProperty(propertyName) as MaterialProperty<T>;
+            } else if (typeof(Color) == typeof(T) ) { 
+                p = mp?.FindColorProperty(propertyName) as MaterialProperty<T>;
+            }
+            if (null==p)
+                return false;
+
+            p.targetValue       = obj;
             p.overrideBaseValue = true;
             return true;
         }

f7ce76c720b94426afd09fc4e8f580bcece9f8b7
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-25 11:56:33 +0900

    open API in materialProperties

--- Runtime/MaterialProperties.cs
+++ Runtime/MaterialProperties.cs
@@ -19,5 +19,38 @@ namespace Unity.MaterialSwitch
         [SerializeField] internal List<TextureProperty> textureProperties = new List<TextureProperty>();
         [SerializeField] internal List<FloatProperty> floatProperties = new List<FloatProperty>();
         [SerializeField] internal bool needsUpdate = true;
+
+        /// <summary>
+        /// Enumerate all texture properties
+        /// </summary>
+        /// <returns>A texture property</returns>
+        public IEnumerable<TextureProperty> GetTextureProperties() 
+        {
+            foreach (TextureProperty t in textureProperties)
+                yield return t;
+        }
+
+        /// <summary>
+        /// Enumerate all float properties
+        /// </summary>
+        /// <returns>A float property</returns>
+        public IEnumerable<FloatProperty> GetFloatProperties() 
+        {
+            foreach (FloatProperty f in floatProperties)
+                yield return f;
+        }
+        
+
+        /// <summary>
+        /// Enumerate all color properties
+        /// </summary>
+        /// <returns>A color property</returns>
+        public IEnumerable<ColorProperty> GetColorProperties() 
+        {
+            foreach (ColorProperty c in colorProperties)
+                yield return c;
+        }
+        
+        
     }
 }

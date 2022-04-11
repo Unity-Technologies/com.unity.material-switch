911a8618dd69b042526406c88e2693e2950acea7
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-04-11 21:14:23 +0900

    add MaterialSwitchClipboardItem

---
+++ Editor/MaterialSwitchClipboardItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Unity.MaterialSwitch
+{
+
+/// <summary>
+/// Clipboard data that is used for copy/paste material properties of MaterialSwitchClip
+/// </summary>
+public class MaterialPropertiesClipboardData
+{    
+    internal MaterialSwitchClip clip;
+    internal int                materialIndex;
+
+    internal MaterialPropertiesClipboardData(MaterialSwitchClip c, int matIndex) {
+        clip          = c;
+        materialIndex = matIndex;
+    }
+    
+}
+
+} //end namespace

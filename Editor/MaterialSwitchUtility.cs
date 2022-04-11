dbf64199ecf693326e3c589b5443ec875501ec6c
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-04-11 17:20:46 +0900

    make MaterialSwitchUtility to obsolete

---
+++ Editor/MaterialSwitchUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine.Timeline;
+
+namespace Unity.MaterialSwitch
+{
+    /// <summary>
+    /// Utility class to perform various functions on MaterialSwitch classes.
+    /// </summary>
+    [Obsolete("Replaced by MaterialSwitchEditorUtility")] 
+    public static class MaterialSwitchUtility
+    {
+        /// <summary>
+        /// Init a MaterialSwitchClip 
+        /// </summary>
+        /// <param name="clip">The clip to be initialized</param>
+        [CanBeNull]
+        [Obsolete("Replaced by MaterialSwitchEditorUtility.InitMaterialSwitchClip")] 
+        public static MaterialSwitchClip InitMaterialSwitchClip(TimelineClip clip, TrackAsset track) {
+            return MaterialSwitchEditorUtility.InitMaterialSwitchClip(clip, track);
+        }       
+    }
+}

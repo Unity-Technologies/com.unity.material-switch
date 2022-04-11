b4aee8cd4039f9ea062b620a1f01ee364a695992
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-04-11 23:12:52 +0900

    check copy and paste

--- Tests/Editor/MaterialSwitchClipTests.cs
+++ Tests/Editor/MaterialSwitchClipTests.cs
@@ -39,14 +39,19 @@ internal class MaterialSwitchClipTests
         yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
 
         //Override
-        Color targetColor = Color.blue;
-        Color targetEmissionColor = Color.green;
-
-        bool overrideResult = msClip0.OverrideProperty(mat, "_Color", targetColor);
-        Assert.IsTrue(overrideResult);
+        Color  targetColor         = Color.blue;
+        Color  targetEmissionColor = Color.green;
+        string colorPropertyName   = "_Color";
+        string emissionColorPropertyName   = "_EmissionColor";
         
-        ColorProperty colorProp = msClip0.materialPropertiesList[0].FindColorProperty("_Color");
-        colorProp.overrideBaseValue
+        VerifyOverrideColor(msClip0, mat, colorPropertyName, targetColor);
+        VerifyOverrideColor(msClip0, mat, emissionColorPropertyName, targetEmissionColor);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Copy and paste
+        MaterialPropertiesClipboardData clipboardData = MaterialPropertiesClipboardData.Create(msClip0, 0);
+        // ColorProperty                   colorProp     = msClip0.materialPropertiesList[0].FindColorProperty("_Color");
+        // colorProp.overrideBaseValue
         
         // foreach (var a in msClip0.GetMaterialProperties()) {
         //     foreach (var b in a.GetColorProperties()) {
@@ -54,15 +59,32 @@ internal class MaterialSwitchClipTests
         //         
         //     }
         // }
-        
-        
+
+        const int TARGET_MAT_INDEX = 0;
         TimelineClip       clip1   = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);
-        MaterialSwitchClip msClip1 = clip1.asset as MaterialSwitchClip; 
+        MaterialSwitchClip msClip1 = clip1.asset as MaterialSwitchClip;
         Assert.IsNotNull(msClip1);
+        clipboardData.PasteInto(msClip1,TARGET_MAT_INDEX);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Check
+        VerifyTargetColor(msClip1, TARGET_MAT_INDEX, colorPropertyName, targetColor);
+        VerifyTargetColor(msClip1, TARGET_MAT_INDEX, emissionColorPropertyName, targetEmissionColor);
+        
         
     }   
     
-//----------------------------------------------------------------------------------------------------------------------    
+//----------------------------------------------------------------------------------------------------------------------
+
+    static void VerifyOverrideColor(MaterialSwitchClip msClip, Material mat, string propertyName, Color targetColor) {
+        bool overrideResult = msClip.OverrideProperty(mat, propertyName, targetColor);
+        Assert.IsTrue(overrideResult);        
+    }
+
+    static void VerifyTargetColor(MaterialSwitchClip msClip, int matIndex, string propertyName, Color color) {
+        Assert.AreEqual(color, msClip.materialPropertiesList[matIndex].FindColorProperty(propertyName).GetTargetValue());
+        
+    }
     
 }
 

e2a8ce7459163f3ed05f70c4b45672ffd559e58e
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-04-11 22:36:06 +0900

    Use the new function in MaterialSwitchTrackTests
    (cherry picked from commit f96dfba1845aebe66f94846be6e12182ca3874db)

--- Tests/Editor/MaterialSwitchTrackTests.cs
+++ Tests/Editor/MaterialSwitchTrackTests.cs
@@ -16,10 +16,12 @@ internal class MaterialSwitchTrackTests
 
     [UnityTest]
     public IEnumerator CreateEmptyPlayableAsset() {
-        PlayableDirector director = InitDirector();  
-        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
         
-        TimelineAsset timelineAsset = director.playableAsset as TimelineAsset;        
+        PlayableDirector director = MaterialSwitchEditorTestUtility.CreateDirectorWithTimelineAsset(
+            out TimelineAsset timelineAsset
+        );  
+        
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);        
         TimelineEditorUtility.CreateTrackAndClip<MaterialSwitchTrack, MaterialSwitchClip>(timelineAsset, "TestTrack");
         yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
 
@@ -45,29 +47,18 @@ internal class MaterialSwitchTrackTests
     [UnityTest]
     public IEnumerator AssignSelectionGroupToTrack() {
         
-        PlayableDirector director = InitDirector();  
+        PlayableDirector director = MaterialSwitchEditorTestUtility.CreateDirectorWithTimelineAsset(
+            out TimelineAsset timelineAsset
+        );  
+        TimelineEditorUtility.SelectDirectorInTimelineWindow(director);
         yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
         
-        TimelineAsset timelineAsset = director.playableAsset as TimelineAsset;
-        Assert.IsNotNull(timelineAsset);
         MaterialSwitchTrack track = timelineAsset.CreateTrack<MaterialSwitchTrack>(null, "TestTrack");
 
         SelectionGroup group = SelectionGroupManager.GetOrCreateInstance().CreateSelectionGroup("New Group", Color.green);
         director.SetGenericBinding(track, group);
         TimelineClip clip = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);            
         yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------    
-
-
-    PlayableDirector InitDirector() {
-        TimelineAsset    timelineAsset = ScriptableObject.CreateInstance<TimelineAsset>();
-        PlayableDirector director      = new GameObject("Director").AddComponent<PlayableDirector>();  
-        
-        director.playableAsset = timelineAsset;
-        TimelineEditorUtility.SelectDirectorInTimelineWindow(director);
-        return director;
     }
     
 }

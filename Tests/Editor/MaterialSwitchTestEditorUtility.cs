84cf07c46d4b30832385a64abcb40edfda97e1f9
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-04-11 22:32:48 +0900

    Add MaterialSwitchTestEditorUtility
    (cherry picked from commit c5d600c59e4a2bfeb1fc28cae1c73dbd24dc5a58)

---
+++ Tests/Editor/MaterialSwitchTestEditorUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Unity.MaterialSwitch.EditorTests
+{
+internal static class MaterialSwitchEditorTestUtility
+{
+    private static PlayableDirector CreateDirectorWithTimelineAsset(string candidatePath, 
+        out TimelineAsset timelineAsset) 
+    {
+        timelineAsset = ScriptableObject.CreateInstance<TimelineAsset>();
+        PlayableDirector director = new GameObject("Director").AddComponent<PlayableDirector>();
+        Assert.IsNotNull(timelineAsset);
+
+        director.playableAsset = timelineAsset;
+        return director;
+    }
+    
+}
+
+} //end namespace

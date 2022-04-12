3760610459b4beebae8c9521f56f40e65d6a543c
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-04-12 13:02:19 +0900

    api: open API to copy and paste material properties (#95)
    * add initial api to copy/paste material switch clip properties
    * add and use MaterialPropertiesClipboardData to copy/paste
    * add MaterialSwitchClipTests to check copy/paste result
    * add validity checks when pasting MaterialPropertiesClipboardData

---
+++ Editor/MaterialPropertiesClipboardData.cs
@@ -0,0 +1,94 @@
+﻿using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Unity.MaterialSwitch
+{
+
+/// <summary>
+/// Clipboard data that is used for copy/paste material properties of MaterialSwitchClip
+/// </summary>
+public class MaterialPropertiesClipboardData
+{    
+
+    MaterialPropertiesClipboardData(MaterialSwitchClip clip, int matIndex) {
+        m_clip          = clip;
+        m_materialIndex = matIndex;
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    
+    /// <summary>
+    /// Create material properties clipboard data of MaterialSwitchClip that can be pasted on another MaterialSwitchClip  
+    /// </summary>
+    /// <param name="src">The MaterialSwitchClip source</param>
+    /// <param name="materialIndex">The index of the material in the clip. Index less than 0 means global properties
+    /// </param>
+    /// <returns>Material properties data that can be pasted on another MaterialSwitchClip</returns>
+    [NotNull]
+    public static MaterialPropertiesClipboardData Create(MaterialSwitchClip src, int materialIndex) {
+        Assert.IsNotNull(src);
+        return new MaterialPropertiesClipboardData(Object.Instantiate(src), materialIndex);
+    }
+    
+    /// <summary>
+    /// Paste this MaterialPropertiesClipboardData into a certain material in the target MaterialSwitchClip.  
+    /// </summary>
+    /// <param name="target">The MaterialSwitchClip target</param>
+    /// <param name="targetMaterialIndex">
+    ///     The index of the material in the target clip.
+    ///     Index less than 0 means global properties.
+    /// </param>
+    /// <returns>true if the paste is successful, false otherwise</returns>
+    public bool PasteInto(MaterialSwitchClip target, int targetMaterialIndex) {
+            
+        string json = ConvertClipboardDataToJson(this);
+        if (null == json)
+            return false;
+
+        Undo.RecordObject(target, "Paste");
+        
+        //negative targetIndex is reserved for global properties
+        if (IsGlobalMaterialProperty(targetMaterialIndex)) {
+            EditorJsonUtility.FromJsonOverwrite(json, target.globalMaterialProperties);
+            return true;
+        } 
+
+        if (targetMaterialIndex >= target.materialPropertiesList.Count)
+            return false;
+        
+        //preserve material reference, this is not normally changed.
+        Material  oldMaterial = target.materialPropertiesList[targetMaterialIndex].material;
+
+        EditorJsonUtility.FromJsonOverwrite(json, target.materialPropertiesList[targetMaterialIndex]);
+        target.materialPropertiesList[targetMaterialIndex].material = oldMaterial;
+        return true;
+    }
+
+    [CanBeNull]
+    static string ConvertClipboardDataToJson(MaterialPropertiesClipboardData clipboardData) {
+        int                matIndex = clipboardData.m_materialIndex;
+        MaterialSwitchClip clip     = clipboardData.m_clip;
+        if (IsGlobalMaterialProperty(matIndex))
+            return EditorJsonUtility.ToJson(clip.globalMaterialProperties);
+
+        if (matIndex >= clip.materialPropertiesList.Count)
+            return null;
+        
+        return EditorJsonUtility.ToJson(clip.materialPropertiesList[matIndex]);
+    }
+
+    static bool IsGlobalMaterialProperty(int index) {
+        return index < 0;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    readonly MaterialSwitchClip m_clip;
+    readonly int                m_materialIndex;
+
+    
+}
+
+} //end namespace
---
+++ Tests/Editor/MaterialSwitchClipTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using Unity.FilmInternalUtilities.Editor;
+using Unity.SelectionGroups;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.TestTools;
+using UnityEngine.Timeline;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Unity.MaterialSwitch.EditorTests
+{
+internal class MaterialSwitchClipTests
+{
+    
+    [UnityTest]
+    public IEnumerator CopyAndPasteMaterialProperties() {
+        
+        PlayableDirector director = MaterialSwitchEditorTestUtility.CreateDefaultDirectorAndTrack(
+            out TimelineAsset _, out MaterialSwitchTrack track, out SelectionGroup group
+        );  
+        TimelineEditorUtility.SelectDirectorInTimelineWindow(director);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
+
+        //Add Sphere and material
+        MeshRenderer mr = CreateNewSphereWithMaterialInGroup(group);
+        Material mat = mr.sharedMaterial;
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+        
+        TimelineClip       clip0   = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);
+        MaterialSwitchClip msClip0 = clip0.asset as MaterialSwitchClip;
+        Assert.IsNotNull(msClip0);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Override
+        Color  targetColor         = Color.blue;
+        Color  targetEmissionColor = Color.green;
+        string colorPropertyName   = "_Color";
+        string emissionColorPropertyName   = "_EmissionColor";
+        
+        VerifyOverrideColor(msClip0, mat, colorPropertyName, targetColor);
+        VerifyOverrideColor(msClip0, mat, emissionColorPropertyName, targetEmissionColor);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Copy and paste
+        MaterialPropertiesClipboardData clipboardData = MaterialPropertiesClipboardData.Create(msClip0, 0);
+
+        const int TARGET_MAT_INDEX = 0;
+        TimelineClip       clip1   = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);
+        MaterialSwitchClip msClip1 = clip1.asset as MaterialSwitchClip;
+        Assert.IsNotNull(msClip1);
+        Assert.IsTrue(clipboardData.PasteInto(msClip1,TARGET_MAT_INDEX));
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Check
+        VerifyTargetColor(msClip1, TARGET_MAT_INDEX, colorPropertyName, targetColor);
+        VerifyTargetColor(msClip1, TARGET_MAT_INDEX, emissionColorPropertyName, targetEmissionColor);
+        
+    }   
+
+//----------------------------------------------------------------------------------------------------------------------
+    
+    [UnityTest]
+    public IEnumerator CheckMaterialPropertiesCopyAndPasteValidity() {
+        
+        PlayableDirector director = MaterialSwitchEditorTestUtility.CreateDefaultDirectorAndTrack(
+            out TimelineAsset _, out MaterialSwitchTrack track, out SelectionGroup group
+        );  
+        TimelineEditorUtility.SelectDirectorInTimelineWindow(director);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(3);
+
+        //Add Sphere and material
+        CreateNewSphereWithMaterialInGroup(group);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+        
+        TimelineClip       clip0   = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);
+        MaterialSwitchClip msClip0 = clip0.asset as MaterialSwitchClip;
+        Assert.IsNotNull(msClip0);
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+
+        //Copy and paste
+        int numMaterialProperties = msClip0.materialPropertiesList.Count;
+        MaterialPropertiesClipboardData validClipboardData = MaterialPropertiesClipboardData.Create(msClip0, 0);
+        MaterialPropertiesClipboardData invalidClipboardData = MaterialPropertiesClipboardData.Create(msClip0, numMaterialProperties);
+
+        TimelineClip       clip1   = TimelineEditorReflection.CreateClipOnTrack(typeof(MaterialSwitchClip), track, 0);
+        MaterialSwitchClip msClip1 = clip1.asset as MaterialSwitchClip;
+        Assert.IsNotNull(msClip1);
+        Assert.IsTrue(validClipboardData.PasteInto(msClip1,0));
+        Assert.IsFalse(validClipboardData.PasteInto(msClip1,numMaterialProperties));
+        Assert.IsFalse(invalidClipboardData.PasteInto(msClip1,0));
+        Assert.IsFalse(invalidClipboardData.PasteInto(msClip1,numMaterialProperties));
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(1);
+        
+    }   
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    static void VerifyOverrideColor(MaterialSwitchClip msClip, Material mat, string propertyName, Color targetColor) {
+        bool overrideResult = msClip.OverrideProperty(mat, propertyName, targetColor);
+        Assert.IsTrue(overrideResult);        
+    }
+
+    static void VerifyTargetColor(MaterialSwitchClip msClip, int matIndex, string propertyName, Color color) {
+        Assert.AreEqual(color, msClip.materialPropertiesList[matIndex].FindColorProperty(propertyName).GetTargetValue());        
+    }
+
+    static MeshRenderer CreateNewSphereWithMaterialInGroup(SelectionGroup group) {
+        GameObject   sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        MeshRenderer mr     = sphere.GetComponent<MeshRenderer>();
+        Material mat = new Material(mr.sharedMaterial) {
+            name = "TestMaterial"
+        };
+        mr.material = mat;
+        group.Add(sphere);
+        return mr;
+    }
+    
+}
+
+} //end namespace

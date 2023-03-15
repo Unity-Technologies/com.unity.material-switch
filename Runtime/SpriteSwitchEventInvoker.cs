ed10db874b876985bd2af83297c44464ce72d585
Simon Wittber <simonwittber@unity3d.com>
2023-03-15 09:38:28 +0800

    use MonoBehaviourSingleton.

--- Runtime/SpriteSwitchEventInvoker.cs
+++ Runtime/SpriteSwitchEventInvoker.cs
@@ -1,33 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.FilmInternalUtilities;
 using UnityEngine;
 
 namespace Unity.MaterialSwitch
 {
     [AddComponentMenu("")]
     [ExecuteAlways]
-    internal class SpriteSwitchEventInvoker : MonoBehaviour
+    internal class SpriteSwitchEventInvoker : MonoBehaviourSingleton<SpriteSwitchEventInvoker>
     {
-        private static SpriteSwitchEventInvoker _instance;
-
-        private static SpriteSwitchEventInvoker Instance
-        {
-            get
-            {
-                if(_instance == null)
-                {
-                    _instance = new GameObject().AddComponent<SpriteSwitchEventInvoker>();
-                    _instance.gameObject.hideFlags = HideFlags.HideAndDontSave;
-                }
-                return _instance;
-            }
-        }
 
         private List<System.Action> actions = new List<Action>();
 
         public static void OnLateUpdate(System.Action action)
         {
-            Instance.actions.Add(action);
+            GetOrCreateInstance().actions.Add(action);
         }
 
         void LateUpdate()

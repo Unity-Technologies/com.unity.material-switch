28bb57faa3c57ec537630cb0e930681535d55c26
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-03-18 20:37:14 +0900

    fix: hide SpriteSwitchEventInvoker gameObject

--- Runtime/SpriteSwitchEventInvoker.cs
+++ Runtime/SpriteSwitchEventInvoker.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//[Note-sin: 2023-03-18] Comment this for debugging in the editor
+//#define DEBUG_SPRITE_SWITCH
+
+using System;
 using System.Collections.Generic;
 using Unity.FilmInternalUtilities;
 using UnityEngine;
@@ -11,6 +14,14 @@ namespace Unity.MaterialSwitch
     {
 
         private List<System.Action> actions = new List<Action>();
+
+        protected override void AwakeInternalV() { 
+#if DEBUG_SPRITE_SWITCH
+            gameObject.hideFlags = HideFlags.None;
+#else
+            gameObject.hideFlags = HideFlags.HideInHierarchy;
+#endif
+        }
 
         public static void OnLateUpdate(System.Action action)
         {
